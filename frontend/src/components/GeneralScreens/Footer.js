b13f00f2ecef209e7a1f1ffbfdd4b1dafd7ff549
MrJefferson29 <thejefferson29@gmail.com>
2024-03-25 12:51:48 +0100

    first commit

---
+++ frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+import "../../Css/Footer.css";
+import { Col, Container, NavLink, Row } from "react-bootstrap";
+import { faTruckRampBox } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <Styles>
+      <Container style={{ padding: "65px" }}>
+        <Row>
+          <Col md="3">
+            <Link to="/registermajiggy" style={{ textDecoration: "none" }}>
+              <div className="IconWrapper">
+                <FontAwesomeIcon icon={faTruckRampBox} />
+              </div>
+            </Link>
+          </Col>
+          <center>
+            <Col md="9">
+              <font
+                style={{
+                  fontSize: "2.3rem",
+                  fontWeight: "600",
+                  matginBottom: "25px",
+                }}
+              >
+                Bernard's Courier
+              </font>
+            </Col>
+          </center>
+        </Row>
+        <Row>
+          <Col md="6">
+            <center>
+              <p
+                style={{
+                  fontSize: "2.3rem",
+                  fontWeight: "200",
+                  color: "white",
+                }}
+              >
+                Location
+              </p>
+              <div style={{ color: "burlywood", display: "flex" }}>
+                <Col>
+                  <p style={{ fontSize: "1.2rem" }}>Lexington st, Soho</p>
+                  <p style={{ fontSize: "1.2rem" }}>London, UK</p>
+                </Col>
+                <Col>
+                  <p style={{ fontSize: "1.2rem" }}>Cypher st, Boston</p>
+                  <p style={{ fontSize: "1.2rem" }}>Massachusetts, US</p>
+                </Col>
+              </div>
+            </center>
+          </Col>
+          <Col md="4">
+            <center>
+              <p style={{ fontSize: "2.3rem", fontWeight: "200" }}>Contact</p>
+              <NavLink
+                to="bcs.courier.help@gmail.com"
+                className="lin"
+                style={{ fontSize: "1.2rem", color: "burlywood" }}
+              >
+                bcs.courier.help@gmail.com
+              </NavLink>
+            </center>
+          </Col>
+          <Col md="2">
+            <p style={{ fontSize: "2.3rem", fontWeight: "200" }}>Join Us</p>
+            <NavLink
+              to="#"
+              className="lin"
+              style={{
+                fontSize: "1.2rem",
+                color: "burlywood",
+                borderBottom: "2px solid burlywood",
+                padding: "2px",
+                width: "auto",
+                margin: "10px",
+              }}
+            >
+              Customer Support
+            </NavLink>
+          </Col>
+        </Row>
+      </Container>
+    </Styles>
+  );
+};
+
+export default Footer;
+
+const Styles = styled.div`
+  overflow-x: hidden;
+  background: black;
+  color: white;
+  .IconWrapper {
+    width: auto;
+    font-size: 56px;
+    color: sandybrown;
+    padding-right: 20px;
+    transition: transform 0.2s ease;
+  }
+`;
---
+++ frontend/src/components/GeneralScreens/Header.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../../Css/Header.css";
+import { AuthContext } from "../../Context/AuthContext";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+import { faDollyBox } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  const bool = localStorage.getItem("authToken") ? true : false;
+  const [auth, setAuth] = useState(bool);
+  const { activeUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setAuth(bool);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1600);
+  }, [bool]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
+
+  return (
+    <Styles>
+      <Navbar collapseOnSelect expand="lg" className="navy" >
+        <Container >
+          <Navbar.Brand href="/">
+            <div className="IconWrapper">
+              <FontAwesomeIcon icon={faDollyBox} />
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            style={{ position: "relative", background: "aliceblue" }}
+          />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            sticky="top"
+            className="justify-content-end"
+          >
+            <Nav className="justify-content-end">
+              <Link
+                className="link"
+                to="/"
+                style={{ color: "aliceblue" }}
+              >
+                HOME
+              </Link>
+              <Link
+                className="link"
+                to="/tracking"
+                style={{ color: "aliceblue" }}
+              >
+                TRACK PACKAGE
+              </Link>
+              {auth ? (
+                <Link
+                  className="link"
+                  to="/create-post"
+                  style={{ color: "aliceblue" }}
+                >
+                  NEW PACKAGE
+                </Link>
+              ) : (
+                <></>
+              )}
+              <Link
+                className="link"
+                to="/about"
+                style={{ color: "aliceblue" }}
+              >
+                ABOUT US
+              </Link>
+              <Link
+                className="link"
+                to="/get-delivery-quote"
+                style={{ color: "aliceblue" }}
+              >
+                GET QUOTE
+              </Link>
+              <Link
+                className="link"
+                to="/terms-and-privacy-policy"
+                style={{ color: "aliceblue" }}
+              >
+                OUR POLICIES
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Styles>
+  );
+};
+
+export default Header;
+
+const Styles = styled.div`
+  // height: 200px;
+  .IconWrapper {
+    font-size: 56px;
+    color: sandybrown;
+    padding-right: 20px;
+    transition: transform 0.2s ease;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+  .link {
+    font-size: 1.2rem;
+    text-decoration: none;
+    font-weight: bold;
+    color: aliceblue;
+    margin-left: 45px;
+  }
+  .eor {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
